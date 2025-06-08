136d3ce5a5d0225541320d1f8b81fe307f7e87f4
Anshul Dwivedi <anshuldwivedi012@gmail.com>
2025-06-08 19:34:10 +0530

    Testimonials added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./section/Hero";
 import ShowcaseSection from "./section/ShowcaseSection";
 import FeatureCards from "./section/FeatureCards";
 import Experience from "./section/Experience";
+import TechStack from "./section/TechStack";
+import Testimonials from "./section/Testimonals";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
       <LogoShowcase />
       <FeatureCards />
       <Experience />
+      <TechStack />
+      <Testimonials />
     </>
   );
 }
